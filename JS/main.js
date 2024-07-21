b7cb9129be0735a0e738d9de381f5a78df94158f
Eng-Mahmoud Gaber <107422408+mgaber200089@users.noreply.github.com>
2024-07-21 14:44:24 -0700

    m

--- JS/main.js
+++ JS/main.js
@@ -1,16 +1,20 @@
-const toggleMenuClicked = () => {
-    const body = document.body;
-    const openIcon = document.getElementById("open-icon");
-    const closeIcon = document.getElementById("close-icon");
-  
-    body.classList.toggle("open");
-  
-    if (body.classList.contains("open")) {
-      openIcon.style.display = "none";
-      closeIcon.style.display = "flex";
-    } else {
-      openIcon.style.display = "flex";
-      closeIcon.style.display = "none";
-    }
-  };
-  
+const header = document.querySelector("header");
+
+window.addEventListener ("scroll" , function() {
+    header.classList.toggle ("sticky" , window.scrollY > 120);
+});
+
+let menu = document.querySelector('#menu-icon');
+let navlist = document.querySelector('.navlist');
+
+menu.onclick = () => 
+{
+    menu.classList.toggle('bx-x');
+    navlist.classList.toggle('active');
+};
+
+window.onscroll = () => 
+{
+    menu.classList.remove('bx-x');
+    navlist.classList.remove('active');
+};
